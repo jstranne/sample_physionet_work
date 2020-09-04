21f006dadbdd53a9584839405cbcbccdff2c9471
jstranne <jasonstranne@gmail.com>
2020-09-04 03:49:01 +0000

    added the relative_positioning model

--- Stager_net_pratice.py
+++ Stager_net_pratice.py
@@ -5,7 +5,6 @@ from torchsummary import summary
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 256, 3000, 10, 100
 
 # Create random Tensors to hold inputs and outputs
 x = torch.randn(2, 3000)
@@ -36,47 +35,46 @@ class StagerNet(nn.Module):
 
         #we want 2 filters?
         self.conv1 = nn.Conv2d(1, 2, (1, 2), stride=(1, 1))
-
         self.conv2 = nn.Conv2d(1, 16, (50,1), stride=(1,1))
-
         self.conv3 = nn.Conv2d(16, 16, (50,1), stride=(1,1))
-
         self.dense1 = nn.Linear(416,100)
+        self.batchnorm1 = nn.BatchNorm2d(16)
+        self.batchnorm2 = nn.BatchNorm2d(16)
     def forward(self, x):
-        # x = F.relu(self.conv1(x))
-        # return F.log_softmax(x, dim=1)
-        print(x.size())
+        # Input should be C by T (2 by 3000)
+        # change into C by T by 1 extending
+        print("Initial Size", x.size())
+        # convolve with C filters to 1 by T by C
+        #linear activation ??
         x = self.conv1(x)
-        print(x.size())
+        print("After spatial conv", x.size())
+        #permute to C T I
         x = x.permute(0, 3, 2, 1)
-        print(x.size())
+        print("after permutation", x.size())
         x = self.conv2(x)
-        print(x.size())
+        print("After First Temporal Conv", x.size())
         x = F.relu(F.max_pool2d(x, (13,1)))
-        print(x.size())
+        print("Relu and maxpool", x.size())
+        x = self.batchnorm1(x)
+        print("Batchnorm1", x.size())
         x = self.conv3(x)
-        print(x.size())
+        print("After Second Temporal Conv", x.size())
         x = F.relu(F.max_pool2d(x, (13, 1)))
-        print(x.size())
+        print("Relu and maxpool", x.size())
+        x = self.batchnorm2(x)
+        print("Batchnorm2", x.size())
         x = torch.flatten(x,1) #flatten all but batch
-        print(x.size())
+        print("Flattened", x.size())
         x = F.dropout(x, p=0.5)
-        print(x.size())
+        print("dropout", x.size())
         x = self.dense1(x)
-        print(x.size())
+        print("Dense", x.size())
         return x
 
 
 
+    # BatchNorm2d
 
-    # Input should be C by T (2 by 3000)
-
-    # change into C by T by 1 extending
-
-    # convolve with C filters to 1 by T by C
-    #linear activation
-
-    #permute to C T I
 
     # 2D temporal convolution to get C by T by 8
     #Activation is relu, mode same
@@ -101,47 +99,47 @@ class StagerNet(nn.Module):
     # torch.nn.ReLU(),
     # torch.nn.Linear(H, D_out),
 
+if __name__=="__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
+    model = StagerNet().to(device)
+    print(model)
+    # summary(model, (1, 28, 28))
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
-model = StagerNet().to(device)
-print(model)
-# summary(model, (1, 28, 28))
 
+    #we really want 1 channel so we can get the right convolution
+    x = torch.randn(2, 3000)
+    print(x.size())
+    summary(model, (1, 3000, 2))
+    print("test")
 
-#we really want 1 channel so we can get the right convolution
-x = torch.randn(2, 3000)
-print(x.size())
-summary(model, (1, 3000, 2))
-print("test")
-
-# loss_fn = torch.nn.MSELoss(reduction='sum')
-#
-# # Use the optim package to define an Optimizer that will update the weights of
-# # the model for us. Here we will use Adam; the optim package contains many other
-# # optimization algorithms. The first argument to the Adam constructor tells the
-# # optimizer which Tensors it should update.
-# learning_rate = 1e-4
-# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-# for t in range(500):
-#     # Forward pass: compute predicted y by passing x to the model.
-#     y_pred = model(x)
-#
-#     # Compute and print loss.
-#     loss = loss_fn(y_pred, y)
-#     if t % 100 == 99:
-#         print(t, loss.item())
-#
-#     # Before the backward pass, use the optimizer object to zero all of the
-#     # gradients for the variables it will update (which are the learnable
-#     # weights of the model). This is because by default, gradients are
-#     # accumulated in buffers( i.e, not overwritten) whenever .backward()
-#     # is called. Checkout docs of torch.autograd.backward for more details.
-#     optimizer.zero_grad()
-#
-#     # Backward pass: compute gradient of the loss with respect to model
-#     # parameters
-#     loss.backward()
-#
-#     # Calling the step function on an Optimizer makes an update to its
-#     # parameters
-#     optimizer.step()
+    # loss_fn = torch.nn.MSELoss(reduction='sum')
+    #
+    # # Use the optim package to define an Optimizer that will update the weights of
+    # # the model for us. Here we will use Adam; the optim package contains many other
+    # # optimization algorithms. The first argument to the Adam constructor tells the
+    # # optimizer which Tensors it should update.
+    # learning_rate = 1e-4
+    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # for t in range(500):
+    #     # Forward pass: compute predicted y by passing x to the model.
+    #     y_pred = model(x)
+    #
+    #     # Compute and print loss.
+    #     loss = loss_fn(y_pred, y)
+    #     if t % 100 == 99:
+    #         print(t, loss.item())
+    #
+    #     # Before the backward pass, use the optimizer object to zero all of the
+    #     # gradients for the variables it will update (which are the learnable
+    #     # weights of the model). This is because by default, gradients are
+    #     # accumulated in buffers( i.e, not overwritten) whenever .backward()
+    #     # is called. Checkout docs of torch.autograd.backward for more details.
+    #     optimizer.zero_grad()
+    #
+    #     # Backward pass: compute gradient of the loss with respect to model
+    #     # parameters
+    #     loss.backward()
+    #
+    #     # Calling the step function on an Optimizer makes an update to its
+    #     # parameters
+    #     optimizer.step()
